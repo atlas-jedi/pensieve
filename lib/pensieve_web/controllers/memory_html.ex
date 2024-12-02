8deaeb2e1d22b36d1879384634a7c63e35474e38
André Pestana Silva <atlas.ifpa@gmail.com>
2024-12-02 16:40:41 -0300

    feat: extract MemoryHTML.memory_inserted_at/1

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -1,7 +1,13 @@
 defmodule PensieveWeb.MemoryHTML do
+
   use PensieveWeb, :html
 
+  alias Pensieve.Memories.Memory
   import PensieveWeb.MemoryInformation
 
   embed_templates "/memory_html/*"
+
+  def memory_inserted_at(%Memory{inserted_at: timestamp}) do
+    Calendar.strftime(timestamp, "%d/%m/%y %I:%M:%S %p")
+  end
 end
