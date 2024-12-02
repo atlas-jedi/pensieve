a0dc75d2718714e0ea5879e7ede47bd0f1b80b2d
André Pestana Silva <atlas.ifpa@gmail.com>
2024-12-02 19:08:54 -0300

    feat: add 'delete' action

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -8,6 +8,6 @@ defmodule PensieveWeb.MemoryHTML do
   embed_templates "/memory_html/*"
 
   def memory_inserted_at(%Memory{inserted_at: timestamp}) do
-    Calendar.strftime(timestamp, "%d/%m/%y %I:%M:%S %p")
+    Calendar.strftime(timestamp, "%d/%m/%y %H:%M:%S")
   end
 end
