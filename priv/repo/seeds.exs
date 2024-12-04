2433c55e49c53fac9a52aafd7957a125c2d28288
André Pestana Silva <atlas.ifpa@gmail.com>
2024-12-04 14:21:19 -0300

    feat: add seed data with Faker

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Pensieve.Repo
+alias Pensieve.Memories.Memory
+
+for _ <- 0..50 do
+  Repo.insert!(%Memory{title: Faker.Lorem.sentence})
+end
